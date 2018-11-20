7a21e69e23a242191b44cd28c8f4f11d4a057a05
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-11-20 14:04:03 +1100

    Add markup

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,6 +1,42 @@
 import React from 'react'
 
+class Countdown extends React.Component { 
+    
+}
+
 const Countdown = (props) =>
-    <div> <h1>New Year is coming up soon</h1> </div>
+     <section className="hero is-info is-bold is-fullheight">
+        <div className="hero-body">
+            <div className="container">
+            <h1 className="title">New Year is coming up</h1>
+            <nav className="level">
+        <div className="level-item has-text-centered">
+            <div>
+            <p className="heading">Days</p>
+            <p className="title">3,456</p>
+            </div>
+        </div>
+        <div className="level-item has-text-centered">
+            <div>
+            <p className="heading">Hours</p>
+            <p className="title">123</p>
+            </div>
+        </div>
+        <div className="level-item has-text-centered">
+            <div>
+            <p className="heading">Minutes</p>
+            <p className="title">456K</p>
+            </div>
+        </div>
+        <div className="level-item has-text-centered">
+            <div>
+            <p className="heading">Seconds</p>
+            <p className="title">789</p>
+            </div>
+        </div>
+        </nav>
+                    </div>
+                </div>
+            </section>
 
 export default Countdown
