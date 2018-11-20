44d1afaf2fcaab27efc7fe9bdcf2b41cf1f00d7b
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-11-20 14:14:20 +1100

    add moment js

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,42 +1,56 @@
 import React from 'react'
 
-class Countdown extends React.Component { 
-    
+export default class Countdown extends React.Component { 
+    constructor(props) { 
+   // parent constructor to pass the props
+        super(props);
+        this.state = {
+            interval: this.getRemainingTime()
+        }
+        }
+    getRemainingTime() {
+        let now = new Date();
+        let newYear = new Date(now.getFullYear() + 1, 0, 1);
+        
+        }
+    render() { 
+        return (
+        <section className="hero is-info is-bold is-fullheight">
+            <div className="hero-body">
+                <div className="container">
+                        <h1 className="title">New Year is coming up</h1>
+                    <div className="section">
+                    <nav className="level">
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <p className="heading">Days</p>
+                                <p className="title">3,456</p>
+                            </div>
+                        </div>
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <p className="heading">Hours</p>
+                                <p className="title">123</p>
+                            </div>
+                        </div>
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <p className="heading">Minutes</p>
+                                <p className="title">456K</p>
+                            </div>
+                        </div>
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <p className="heading">Seconds</p>
+                                <p className="title">789</p>
+                            </div>
+                        </div>
+                            </nav>
+                        </div>
+                </div>
+            </div>
+            </section>
+        )
+    }
 }
 
-const Countdown = (props) =>
-     <section className="hero is-info is-bold is-fullheight">
-        <div className="hero-body">
-            <div className="container">
-            <h1 className="title">New Year is coming up</h1>
-            <nav className="level">
-        <div className="level-item has-text-centered">
-            <div>
-            <p className="heading">Days</p>
-            <p className="title">3,456</p>
-            </div>
-        </div>
-        <div className="level-item has-text-centered">
-            <div>
-            <p className="heading">Hours</p>
-            <p className="title">123</p>
-            </div>
-        </div>
-        <div className="level-item has-text-centered">
-            <div>
-            <p className="heading">Minutes</p>
-            <p className="title">456K</p>
-            </div>
-        </div>
-        <div className="level-item has-text-centered">
-            <div>
-            <p className="heading">Seconds</p>
-            <p className="title">789</p>
-            </div>
-        </div>
-        </nav>
-                    </div>
-                </div>
-            </section>
-
-export default Countdown
